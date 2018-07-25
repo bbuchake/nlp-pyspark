f090791bdd00935dbc7d2fa84fd7d7514b387823
Bilwa Buchake <bilwa@calvisitor-10-105-157-112.calvisitor.1918.berkeley.edu>
2018-07-24 21:12:08 -0700

    added plots

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,12 +7,37 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/line")
-def test():
+@app.route("/accuracy_plot")
+def accuracy_plot():
+    data_all = pd.read_csv('data/models_analysis.csv')
     data = [{
-        "x": [1, 2, 3, 4, 5],
-        "y": [1, 2, 4, 8, 16] }]
+        "x": data_all.train_test_split.tolist(),
+        "y": data_all.accuracy.tolist(),
+        "color": ['lightgreen' if acc < 94.0 else 'lightblue' if acc >= 94.0 and acc < 97.0 else 'lightpink' for acc in data_all.accuracy.tolist()]
+    }]
 
+    return jsonify(data)
+
+@app.route("/duration_plot")
+def duration_plot():
+    data_all = pd.read_csv('data/models_analysis.csv')
+    data = [{
+        "x": data_all.train_test_split.tolist(),
+        "y": data_all.duration.tolist(),
+        "color": ['green' if dur < 20.0 else 'blue' if dur >= 20.0 and dur < 100.0 else 'red' for dur in data_all.duration.tolist()]}]
+
+    return jsonify(data)
+
+@app.route("/duration_vs_accuracy_plot")
+def duration_vs_accuracy_plot():
+    data_all = pd.read_csv('data/models_analysis.csv')
+    data = [{
+        "x": data_all.accuracy.tolist(),
+        "y": data_all.duration.tolist(),
+        "text": [i for i in data_all.train_test_split.tolist()],
+        "size": [int(i/10) for i in data_all.duration.tolist()],
+        "color": ['green' if dur < 20.0 else 'blue' if dur >= 20.0 and dur < 100.0 else 'red' for dur in data_all.duration.tolist()]
+}]
     return jsonify(data)
 
 if __name__ == "__main__":
